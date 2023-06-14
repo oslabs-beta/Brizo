4d653c6221dc199a671ca23d4be746a500104c42
owen <on-hill@outlook.com>
2023-06-14 17:27:47 -0500

    move client folder

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Router, Routes, Route } from 'react-router-dom';
-import StructureOverview from '/components/StructureOverview';
+import StructureOverview from './components/StructureOverview';
 const App = () => {
   return (
     <Routes>
