97749f98190c6f08be043bbaf4ff611647864384
Cortland Young <dcy.y@outlook.com>
2023-06-17 18:19:29 -0500

    cis tests functional, need additional data parsing to separate by section

--- src/server/controllers/securityController.ts
+++ src/server/controllers/securityController.ts
@@ -1,5 +1,6 @@
 import { Request, Response, NextFunction } from 'express';
 import { exec } from 'child_process';
+import fs from 'fs';
 
 const securityController = {
   runKubeBench: async (req: Request, res: Response, next: NextFunction) => {
@@ -40,14 +41,77 @@ const securityController = {
               return next(error);
             }
 
-            // parse output and store in variable
-            const kubeBenchOutput = JSON.parse(stdout.trim());
+            // trim output and store in variable
+            const kubeBenchOutput = stdout.trim();
 
-            // save kube bench output to res.locals
-            res.locals.kubeBenchOutput = kubeBenchOutput;
+            // write kubeBenchOutput to output.txt file 
+            fs.writeFile('output.txt', kubeBenchOutput, (error) => {
+              if (error) {
+                return next({error: 'Error writing to output file.'});
+              } else console.log('Output file written successfully.');
+            });
 
-            // move to next middleware
-            return next();
+            // store output text file in const 
+            fs.readFile('output.txt', 'utf-8', (error, data) => {
+              if (error) {
+                return next({error: 'Error reading output file.'})
+              }
+              console.log('Output file read successfully.');
+
+              // split data file into array of lines and store it in outputLines const
+              const outputLines = data.split('\n');
+
+              // convert text data to JSON format
+              const jsonOutputLines = outputLines.map((line) => {
+                const [key] = line.split(':');
+                return [key];
+              })
+
+              // initialize testLines const 
+              const testLines: String[][] = [];
+
+              // isolate individual tests from jsonOutputLines
+              jsonOutputLines.forEach(line => {
+                // test lines start with pass/warn/fail in []
+                if (line[0].includes('[PASS]') || 
+                    line[0].includes('[WARN]') || 
+                    line[0].includes('[FAIL]')) {
+                      testLines.push(line);
+                    }
+              });
+              // save testLines on res.locals as allTestResults 
+              // this is an array of test results from all 5 sections 
+              res.locals.allTestResults = testLines;
+
+              // save kube bench output to res.locals
+              res.locals.kubeBenchOutput = jsonOutputLines;
+
+              // initialize index variables to slice sections from testLines
+              let cpsctStart: number = 0;
+              let cpsctEnd: number = 0;
+
+              // isolate test results by section
+              for (let index = 0; index < testLines.length; index += 1) {
+                // SECTION 1: Control Plane Security Configuration
+                // find first index position to slice from testLines array 
+                if (testLines[0][index] !== undefined && 
+                  testLines[0][index].includes('1.1.1')) {
+                  cpsctStart = index;
+                }
+                // find last index position to slice from testLines array 
+                else if (testLines[0][index] !== undefined && 
+                  testLines[0][index].includes('1.4.1')) {
+                  cpsctEnd = index;
+                  console.log('HERE');
+              }
+              }
+              console.log(cpsctStart, cpsctEnd);
+              const controlPlaneSecurityConfigurationTests: Object = {
+                controlPlaneNodeConfigFiles: testLines
+              }
+              // move to next middleware
+              return next();
+            });
           });
         });
       });
