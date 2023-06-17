4573270826d3f02af8ce669b9ebd7224dee7db36
owen <on-hill@outlook.com>
2023-06-17 11:33:18 -0500

    add skeleton css boilerplate

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { createRoot } from 'react-dom/client';
 import App from './App';
+import './css/normalize.css';
+import './css/skeleton.css';
 const domNode = document.getElementById('root')!;
 const root = createRoot(domNode);
 
