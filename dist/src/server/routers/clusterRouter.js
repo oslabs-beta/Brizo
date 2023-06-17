2731bb8768245ca67de8498a416b3ab5aa888e7e
Cortland Young <dcy.y@outlook.com>
2023-06-17 10:38:42 -0500

    dependencies updated

--- dist/src/server/routers/clusterRouter.js
+++ dist/src/server/routers/clusterRouter.js
@@ -4,5 +4,18 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
+var clusterController_1 = __importDefault(require("../controllers/clusterController"));
 var router = express_1.default.Router();
+// get request for namespace list
+router.get('/namespaces', clusterController_1.default.getNamespaces, function (req, res) {
+    res.status(200).json(res.locals.namespaceList);
+});
+// get request for pod list
+router.get('/pod/:namespace', clusterController_1.default.getPods, function (req, res) {
+    res.status(200).json(res.locals.podList);
+});
+// get request for node list
+router.get('/node/:namespace', clusterController_1.default.getNodes, function (req, res) {
+    res.status(200).json(res.locals.nodeList);
+});
 exports.default = router;
