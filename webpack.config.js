500bad3eb94c0f9dd26e1b4d7bf5cf1630b69c81
owen <on-hill@outlook.com>
2023-06-16 20:15:50 -0500

    removed copyplugin and uncommented apirouter

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-// const CopyPlugin = require('copy-webpack-plugin')
 
 module.exports = {
   mode: 'development',
