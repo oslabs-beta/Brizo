3d78b7eda97fc255ad22de80b69600ce2a77bf40
owen <on-hill@outlook.com>
2023-06-19 11:38:42 -0500

    move footer inside of index.html to solve pos issue

--- src/client/components/MainContainer.tsx
+++ src/client/components/MainContainer.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 import NavbarComponent from './NavbarComponent'
-
 export default function MainContainer() {
   return (
     <>
