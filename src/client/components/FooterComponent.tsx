963cc4a20dfeb5eb7d8c1200012c9a2554a0215f
owen <on-hill@outlook.com>
2023-06-19 09:13:03 -0500

    Add footer skelenton

---
+++ src/client/components/FooterComponent.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function FooterComponent() {
+  return (
+    <footer>
+      <div className="footer-content">
+        <a href="https://github.com/oslabs-beta/Brizo">github</a> • <a href="#">linkedin</a>
+      </div>
+    </footer>
+  )
+}
+
+export default FooterComponent
