4bcc811e382aeee66ce945b629da2d211dc8b5ab
owen <on-hill@outlook.com>
2023-06-21 18:34:22 -0500

    flesh out pod logic, start comments, add types

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-// import { render } from 'react-dom';
-// import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './css/normalize.css';
 import './css/skeleton.css';
@@ -12,10 +10,3 @@ const root = createRoot(domNode);
 root.render(
   <App />
 );
-
-// render(
-//   <BrowserRouter>
-//     <App />
-//   </BrowserRouter>,
-//   document.getElementById('root'),
-// );
