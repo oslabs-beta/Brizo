6ee1253c2a6524d11c5869ed627a0f479d243500
Cortland Young <dcy.y@outlook.com>
2023-06-25 15:10:54 -0500

    security router and controller updated for EKS. Router now uses 2 different routes for local/eks clusters

--- src/server/controllers/securityController.ts
+++ src/server/controllers/securityController.ts
@@ -5,16 +5,17 @@ import fs from 'fs';
 // type imports
 import type {
   allTestInfoType,
+  allTestInfoEKSType,
   indexObjectType,
   sectionResultsInfo
 } from '../../../types';
 
 const securityController = {
   // this method runs kube bench tool for cis testing, writes the log to output.text, and sends the info to the front end
-  runKubeBench: async (req: Request, res: Response, next: NextFunction) => {
+  runKubeBenchLocal: async (req: Request, res: Response, next: NextFunction) => {
     try {
       // create kube bench job
-      await securityController.applyKubeBenchJob(next);
+      await securityController.applyKubeBenchJob('tests/cis/job.yaml', next);
 
       // get pod name of kube bench job
       const podName = await securityController.getKubeBenchPodName();
@@ -28,7 +29,7 @@ const securityController = {
       securityController.writeOutputData(kubeBenchOutput, next);
 
       // parse kube bench pod log data before sending to front end
-      const allTestInfo = securityController.parseOutputData(
+      const allTestInfo = securityController.filterOutputData(
         kubeBenchOutput.split('\n')
       );
 
@@ -43,10 +44,42 @@ const securityController = {
     }
   },
 
+  runKubeBenchEKS: async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      // create kube bench job
+      await securityController.applyKubeBenchJob('tests/cis/job-eks.yaml', next);
+
+      // get pod name of kube bench job
+      const podName = await securityController.getKubeBenchPodName();
+
+      // grab the log of the kube bench pod
+      const kubeBenchOutput = await securityController.getKubeBenchPodLog(
+        podName
+      );
+
+      // write kube bench pod log to output.txt
+      securityController.writeOutputData(kubeBenchOutput, next);
+
+      // filter kube bench pod log data before sending to front end
+      const allTestInfoEKS = securityController.filterOutputDataEKS(
+        kubeBenchOutput.split('\n')
+      );
+
+      // save parsed kube bench pod log data on res.locals
+      res.locals.allTestInfoEKS = allTestInfoEKS;
+
+      // move to next middleware
+      next();
+    } catch (error) {
+      // error handling
+      next(error);
+    }
+  },
+
   // method to apply job
-  applyKubeBenchJob: async (next: NextFunction) => {
+  applyKubeBenchJob: async (jobPath: string, next: NextFunction) => {
     // define command to create kube bench test as a job
-    const command = 'kubectl apply -f tests/cis/job.yaml';
+    const command = `kubectl apply -f ${jobPath}`;
 
     // run command using exec
     exec(command, (error) => {
@@ -122,7 +155,7 @@ const securityController = {
     });
   },
 
-  parseOutputData: (outputData: string[]) => {
+  filterOutputData: (outputData: string[]) => {
     // initialize test results array
     const allTestResults: string[] = [];
 
@@ -390,6 +423,93 @@ const securityController = {
 
     // return allTestInfo object to be sent to front end
     return allTestInfo;
+  },
+
+  filterOutputDataEKS: (outputData: string[]) => {
+    // initialize test results array
+    const allTestResults: string[] = [];
+
+    // iterate through outputData and store test results in test results array
+    outputData.forEach((line) => {
+      if (
+        line.includes('[PASS]') ||
+        line.includes('[WARN]') ||
+        line.includes('[FAIL]')
+      ) {
+        allTestResults.push(line);
+      }
+    });
+
+    // initialize index variables to slice sections from testLines
+    /* initialize outside of for loop so we can access for object assignment below
+      the assigned boolean is used to make sure we find the first index and don't reassign later.
+      for example, if our first test starts with '1.1.1', it would be reassigned on test '1.1.11' later.
+      the assigned property accounts for this
+    */
+
+    // SECTION 4 INDICES
+    const wnscStart: indexObjectType = {
+      position: 0,
+      assigned: false
+    };
+    const wnscEnd: indexObjectType = {
+      position: 0,
+      assigned: false
+    };
+
+    // isolate test results by section -> find index positions to slice test results array
+    for (let index = 0; index < allTestResults.length; index += 1) {
+      // SECTION 4: Worker Node Security Configuration
+      // find first index position to slice from testLines array
+      if (
+        allTestResults[index].includes('3.1.1') &&
+        !wnscStart.assigned
+      ) {
+        wnscStart.position = index;
+        wnscStart.assigned = true;
+      }
+      // find last index position to slice from testLines array
+      if (
+        allTestResults[index].includes('3.3.1') &&
+        !wnscEnd.assigned
+      ) {
+        wnscEnd.position = index;
+        wnscEnd.assigned = true;
+      }
+    }
+
+    // create object to store testResults array, remediations array, and summary array for the given section
+    const workerNodeSecurity: sectionResultsInfo = {
+      testResults: allTestResults.slice(
+        wnscStart.position,
+        wnscEnd.position + 1
+      ),
+      remediations: securityController.condenseRemediations(
+        outputData.slice(
+          outputData.indexOf('== Remediations node =='),
+          outputData.indexOf('== Summary node ==')
+        )
+      ),
+      summary: outputData.slice(
+        outputData.indexOf('== Summary node =='),
+        outputData.indexOf('== Summary node ==') + 5
+      )
+    };
+
+    const totalSummary: string[] = outputData.slice(
+      outputData.indexOf('== Summary total =='),
+      outputData.indexOf('== Summary total ==') + 5
+    );
+
+    // create all test info object to return to runKubeBench to be sent to front end
+    // this object stores all the objects we just created for individual sections
+    const allTestInfoEKS: allTestInfoEKSType = {
+      workerNodeSecurity,
+      totalSummary
+    };
+
+    // return allTestInfo object to be sent to front end
+    return allTestInfoEKS;
   },
 
   condenseRemediations: (remediationsArr: string[]) => {
