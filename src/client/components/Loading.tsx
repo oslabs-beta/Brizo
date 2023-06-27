7ff985709cc6f8e68a09d7a8aa91dc6d8306d16f
owen <on-hill@outlook.com>
2023-06-27 01:11:41 -0500

    create loading gif  component

---
+++ src/client/components/Loading.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Loading = () => <img style={{ opacity: '20%', width: '200px', alignSelf: 'center' }} src='https://i.imgur.com/oAXAtif.gif'/>;
+
+export default Loading;
--- src/client/components/ViewCluster.tsx
+++ src/client/components/ViewCluster.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import type { AxiosResponse, AxiosError } from 'axios';
 import axios from 'axios';
 import GrandCISResults from './GrandCISResults';
 import CISConfigResult from './CISConfigResult';
+import Loading from './Loading';
 
 function ViewCluster () {
   const [totalCISResults, setTotalCISResults] = React.useState<JSX.Element>();
@@ -11,27 +11,58 @@ function ViewCluster () {
   const [etcdNodeConfiguration, setEtcdNodeConfiguration] = React.useState<JSX.Element>();
   const [kubernetesPolicies, setKubernetesPolicies] = React.useState<JSX.Element>();
   const [workerNodeSecurity, setWorkerNodeSecurity] = React.useState<JSX.Element>();
+  const [displayLoadingGif, setDisplayLoadingGif] = React.useState(false);
+
+  const resetResultDisplay = () => {
+    setTotalCISResults(undefined);
+    setControlPlaneConfiguration(undefined);
+    setControlPlaneSecurityConfiguration(undefined);
+    setEtcdNodeConfiguration(undefined);
+    setKubernetesPolicies(undefined);
+    setWorkerNodeSecurity(undefined);
+    setDisplayLoadingGif(false);
+  };
 
   const fetchLocalCISTest = async () => {
     try {
+      // remove previous results
+      resetResultDisplay();
+      // fetch cis test for local cluster and toggle loading gif
+      setDisplayLoadingGif(true);
       const response = await axios.get('/api/security/local/cis');
+      setDisplayLoadingGif(false);
       const data = response.data;
+      // create grand summary component
       setTotalCISResults(<GrandCISResults data={data.totalSummary}/>);
+      // parse data for each component, passing in the test name for the title
       setControlPlaneConfiguration(<CISConfigResult data={data.controlPlaneConfiguration} testName={'Control Plane Configuration'} />);
       setControlPlaneSecurityConfiguration(<CISConfigResult data={data.controlPlaneSecurityConfiguration} testName={'Control Plane Security Configuration'} />);
       setEtcdNodeConfiguration(<CISConfigResult data={data.etcdNodeConfiguration} testName={'Etcd Node Configuration'} />);
       setKubernetesPolicies(<CISConfigResult data={data.kubernetesPolicies} testName={'Kubernetes Policies'} />);
       setWorkerNodeSecurity(<CISConfigResult data={data.workerNodeSecurity} testName={'Worker Node Security'} />);
     } catch (error) {
+      setDisplayLoadingGif(false);
       console.error(error);
     }
   };
 
   const fetchEKSCISTest = async () => {
-    const response = await axios.get('/api/security/eks/cis');
-    const data = response.data;
-    setTotalCISResults(<GrandCISResults data={data.totalSummary} />);
-    setWorkerNodeSecurity(<CISConfigResult data={data.workerNodeSecurity} testName={'Worker Node Security'} />);
+    try {
+      // remove previous results
+      resetResultDisplay();
+      // fetch cis test for eks and toggle loading gif
+      setDisplayLoadingGif(true);
+      const response = await axios.get('/api/security/eks/cis');
+      setDisplayLoadingGif(false);
+      const data = response.data;
+      // create grand summary component
+      setTotalCISResults(<GrandCISResults data={data.totalSummary} />);
+      // parse data for worker node component
+      setWorkerNodeSecurity(<CISConfigResult data={data.workerNodeSecurity} testName={'Worker Node Security'} />);
+    } catch (error) {
+      setDisplayLoadingGif(false);
+      console.error(error);
+    }
   };
 
   return (
@@ -45,6 +76,7 @@ function ViewCluster () {
       </div>
       <hr/>
       <div className='main-info-container'>
+        {displayLoadingGif && <Loading />}
         <div className='cis-container'>
           {totalCISResults}
           {controlPlaneConfiguration}
