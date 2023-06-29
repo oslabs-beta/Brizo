0e711bd1f3abe51dc31ea8483b7645f250f25f4d
owen <on-hill@outlook.com>
2023-06-29 15:05:24 -0500

    move podPhaseStatusToColor function

--- functions.ts
+++ functions.ts
@@ -7,6 +7,14 @@ export const statusToColor = (status: string) => {
   return textColor;
 };
 
+export const podPhaseStatusToColor = (phase: string) => {
+  let textColor;
+  if (phase === 'Running' || phase === 'Succeeded') textColor = 'green';
+  else if (phase === 'Pending') textColor = 'yellow';
+  else textColor = 'red';
+  return textColor;
+};
+
 export const passedNumberFromTest = (resultText: string) => parseInt(resultText);
 
 export const checkStatusFromTest = (resultStatus: string) => resultStatus.match(/[A-Z]+/g)![0];
