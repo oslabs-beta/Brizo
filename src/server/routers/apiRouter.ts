8cde2003636f1e6aaf80d708cd5f6e003d2c3e61
Cortland Young <dcy.y@outlook.com>
2023-06-30 08:17:18 -0500

    updated gitignore

--- src/server/routers/apiRouter.ts
+++ src/server/routers/apiRouter.ts
@@ -1,10 +1,11 @@
 import express from 'express';
 import clusterRouter from './clusterRouter';
 import securityRouter from './securityRouter';
+import prometheusRouter from './prometheusRouter';
 
 const router = express.Router();
 
 router.use('/cluster', clusterRouter);
 router.use('/security', securityRouter);
-
+router.use('/prom', prometheusRouter);
 export default router;
